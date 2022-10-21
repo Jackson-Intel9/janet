ce788ce203ae68795806ad140ab9bfd8b0dc687d
Matthew R. Becker <beckermr@users.noreply.github.com>
2022-10-21 14:14:51 -0500

    Update test_train_network.py

--- janet/tests/test_train_network.py
+++ janet/tests/test_train_network.py
@@ -1,9 +1,13 @@
 """
 """
+import os
 from ..train_network import train_photometry_network
+
+NBATCH = os.environ.get(int("JANET_NBATCH"), 20)
+NSTEPS = os.environ.get(int("JANET_NSTEPS"), 20)
 
 
 def test_train_photometry_network():
-    n_batch, n_steps = 20, 20
+    n_batch, n_steps = NBATCH, NSTEPS
     state, loss_history, p_best, loss_min = train_photometry_network(n_batch, n_steps)
     assert loss_min < loss_history[0]
