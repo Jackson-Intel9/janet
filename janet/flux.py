ce23feb2564b86ca856905a70f29a3a58ac1be3a
zhenming wang <zhenming.wang@intel.com>
2024-05-21 16:09:29 +0800

    Jax upgrade 0.4.25 janet need upgrade api, change jax.numpy.trapz to jax.scipy.integrate.trapezoid

--- janet/flux.py
+++ janet/flux.py
@@ -3,7 +3,7 @@
 from jax import numpy as jnp
 from jax import jit as jjit
 from jax import vmap as jvmap
-
+import jax
 AB0 = 1.13492e-13  # 3631 Jansky placed at 10 pc in units of Lsun/Hz
 
 
@@ -44,7 +44,7 @@ def _obs_flux_ssp(wave_spec_rest, lum_spec, wave_filter, trans_filter, z):
         wave_filter, wave_spec_rest * (1 + z), lum_spec, left=0, right=0
     )
     integrand = trans_filter * lum_zshift_phot / wave_filter
-    lum_filter = jnp.trapz(integrand, x=wave_filter)
+    lum_filter = jax.scipy.integrate.trapezoid(integrand, x=wave_filter)
     return lum_filter
 
 
@@ -52,14 +52,14 @@ def _obs_flux_ssp(wave_spec_rest, lum_spec, wave_filter, trans_filter, z):
 def _rest_flux_ssp(wave_spec_rest, lum_spec, wave_filter, trans_filter):
     lum_phot = jnp.interp(wave_filter, wave_spec_rest, lum_spec, left=0, right=0)
     integrand = trans_filter * lum_phot / wave_filter
-    lum_filter = jnp.trapz(integrand, x=wave_filter)
+    lum_filter = jax.scipy.integrate.trapezoid(integrand, x=wave_filter)
     return lum_filter
 
 
 @jjit
 def _flux_ab0_at_10pc(wave_filter, trans_filter):
     integrand = trans_filter * AB0 / wave_filter
-    lum_ab0_filter = jnp.trapz(integrand, x=wave_filter)
+    lum_ab0_filter = jax.scipy.integrate.trapezoid(integrand, x=wave_filter)
     return lum_ab0_filter
 
 
